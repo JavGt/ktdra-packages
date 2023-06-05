4a1755b200d1dee6a1c4fab9b71d2bc46a547ad6
Javier Gutierrez <82567321+JavGt@users.noreply.github.com>
2023-06-05 10:58:17 -0600

    refactor: cambios en el workspace root

--- scripts/array.mjs
+++ scripts/array.mjs
@@ -1,15 +1,32 @@
 import fs from 'fs/promises';
 import path from 'path';
 import glob from 'glob';
-import { buildPath, packagePath, packageJson, tagLog, lineLog } from './utils/index.mjs';
+import {
+	buildPath,
+	packagePath,
+	packageJson,
+	tagLog,
+	lineLog,
+	srcPath,
+} from './utils/index.mjs';
+import { camelCase } from './utils/camelCase.mjs';
+import { replaceHyphen } from './utils/replaceHyphen.mjs';
 
 const { name } = await packageJson();
 
 /**
- * @typedef {{ folder: string, files: string[] }} FolderAndFiles
+ * @typedef {import("@ktdra-digital/icons/dist/ts").Icon } Icon
+ *
+ * @typedef {{
+ * 	folder: string,
+ * 	icons: {
+ * 		label: string,
+ * 		value: Icon
+ * 	 }[]
+ * }} FolderAndFiles
  * @typedef {FolderAndFiles[]} FoldersAndFiles
  * @typedef {(acc: FoldersAndFiles, file: string) => FoldersAndFiles} ReduceFiles
- * @typedef {(item: FolderAndFiles, folderPath: string)=>Promise<void>} CreateFile
+ * @typedef {(item: FoldersAndFiles, folderPath: string)=>Promise<void>} CreateFile
  */
 
 /**
@@ -18,29 +35,66 @@ const { name } = await packageJson();
  * dentro de esa carpeta tendrá una archivo llamado "index.ts" que exportara un objeto con todos los nombres de los archivos svg
  */
 const run = async () => {
-	const files = glob.sync(`dist/**/*.tsx`, { cwd: packagePath, nodir: true });
+	const files = glob.sync(`svgs/**/*.svg`, {
+		cwd: packagePath,
+	});
 
 	/** @type {FoldersAndFiles} */
 	const foldersAndFiles = files.reduce(
 		/** @type {ReduceFiles} */
 		(acc, file) => {
-			const folder = path.dirname(file).replace('dist/', '');
-			const nameFile = path.basename(file, '.tsx');
-			const index = acc.findIndex(item => item.folder === folder);
+			const folder = path.dirname(file).replace('svgs/', '');
+			const nameFile = path.basename(file, '.svg');
 
-			if (index === -1) acc.push({ folder: folder, files: [nameFile] });
-			else acc[index].files.push(nameFile);
+			const component = camelCase(replaceHyphen(nameFile));
+
+			const label = camelCase(nameFile.replace(/[-_]/g, ' '));
+
+			const index = acc.findIndex((item) => item.folder === folder);
+
+			if (index === -1)
+				acc.push({
+					folder: folder,
+					icons: [
+						{
+							label,
+							value: {
+								name: component,
+								folder,
+							},
+						},
+					],
+				});
+			else acc[index].icons.push({ label, value: { name: component, folder } });
 
 			return acc;
 		},
 		[]
 	);
 
-	await Promise.all(
-		foldersAndFiles.map(async item => {
-			const { folder } = item;
+	const folderPath = path.join(srcPath, 'constants');
+	// Verificar si existe la carpeta utils
 
-			const folderPath = path.join(buildPath, folder, 'utils');
+	const exists = await fs
+		.access(folderPath)
+		.then(() => true)
+		.catch(() => false);
+
+	if (!exists) {
+		try {
+			// crear la carpeta utils
+			await fs.mkdir(folderPath, { recursive: true });
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	await createArrayFile(foldersAndFiles, folderPath);
+
+	return;
+	await Promise.all(
+		foldersAndFiles.map(async (item) => {
+			const { folder } = item;
 
 			try {
 				// crear la carpeta utils
@@ -49,11 +103,9 @@ const run = async () => {
 				console.log(error);
 			}
 
-			await createArrayFile(item, folderPath);
+			// await createBarrelFile(item, folderPath);
 
-			await createBarrelFile(item, folderPath);
-
-			await createTypeFile(item, folderPath);
+			// await createTypeFile(item, folderPath);
 		})
 	);
 
@@ -71,24 +123,30 @@ const run = async () => {
 };
 
 /** @type {CreateFile} */
-const createArrayFile = async (item, folderPath) => {
-	const templateArray = `export const ${item.folder}_ARRAY = [ ${item.files
-		.map(file => `"${file}"`)
-		.join(', ')} ]`;
+const createArrayFile = async (items, folderPath) => {
+	console.log(JSON.stringify(items, null, 2));
+	console.log({ folderPath });
 
+	const template = `export const ICONS = ${JSON.stringify(items)}`;
 	try {
-		await fs.writeFile(path.join(folderPath, 'array.ts'), templateArray, {
+		await fs.writeFile(path.join(folderPath, 'icons.ts'), template, {
 			encoding: 'utf-8',
 		});
 	} catch (error) {
 		console.log(error);
 	}
+	// const templateArray = `export const ${item.folder.replace('/', '_')}_ARRAY = [ ${item.files
+	// 	.map((file) => {
+	// 		return `{ name: '${file.name}', component: '${file.component}' }`;
+	// 	})
+	// 	.join(', ')} ]`;
+	//
 };
 
 /** @type {CreateFile} */
 const createTypeFile = async (item, folderPath) => {
 	const templateType = `export type ${item.folder} = ${item.files
-		.map(file => `"${file}"`)
+		.map((file) => `"${file}"`)
 		.join(' | ')}`;
 
 	try {
@@ -107,7 +165,7 @@ const createBarrelFile = async (_, folderPath) => {
 	const isFileTs = /^(?!.*\.d\.ts$).*\.ts$/;
 
 	const barrel = folders
-		.map(folder => {
+		.map((folder) => {
 			if (folder.startsWith('index')) return '';
 
 			return isFileTs.test(folder)

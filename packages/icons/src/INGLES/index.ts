cf8f92d9ad672b79b406b5344539a5e7dd54fb6d
Javier Gutierrez <82567321+JavGt@users.noreply.github.com>
2023-06-22 11:51:24 -0600

    feat(icons): agregacion de iconos de ingles

---
+++ packages/icons/src/INGLES/index.ts
@@ -0,0 +1,5 @@
+export { default as GrammarNote } from './GrammarNote';
+export { default as Listening } from './Listening';
+export { default as Reading } from './Reading';
+export { default as Speaking } from './Speaking';
+export { default as Writing } from './Writing';
--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -3,6 +3,7 @@ export * as DGB from './DGB';
 export * as KTDRA from './KTDRA';
 export * as NEM from './NEM';
 export * as COMMON from './COMMON';
+export * as INGLES from './INGLES';
 export * from "./hooks"
 export * from './types';
 export * from './utils';
